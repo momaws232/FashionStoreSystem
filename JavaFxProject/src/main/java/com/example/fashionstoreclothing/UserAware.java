1193a21a5b3870d93492a64a7f1a604489e26832
Ahmed Tamer <153434012+momaws232@users.noreply.github.com>
2025-05-21 23:30:55 +0300

    Delete JavaFxProject directory

--- JavaFxProject/src/main/java/com/example/fashionstoreclothing/UserAware.java
+++
@@ -1,5 +0,0 @@
-package com.example.fashionstoreclothing;
-
-public interface UserAware {
-    void setCurrentUser(User user);
-}
--- JavaFxProject/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.javafxproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-    opens com.example.fashionstoreclothing to javafx.fxml;
-    exports com.example.fashionstoreclothing;
-}
